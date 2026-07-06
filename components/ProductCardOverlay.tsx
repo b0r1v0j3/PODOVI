a5015f0e3dc48f13128c51f19fae9568c49dbcb5
Borivoje <borivoje@podovi.online>
2026-07-06 21:16:33 +0200

    feat: redesign homepage catalog shell

--- components/ProductCardOverlay.tsx
+++ components/ProductCardOverlay.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Product } from '@/types';
-import CompareButton from './CompareButton';
 import FavoriteButton from './FavoriteButton';
 
 interface ProductCardOverlayProps {
@@ -12,7 +11,6 @@ export default function ProductCardOverlay({ product }: ProductCardOverlayProps)
     return (
         <div className="absolute top-3 right-3 z-10 flex flex-col gap-1.5">
             <FavoriteButton productId={product.id} size="sm" />
-            <CompareButton product={product} size="sm" />
         </div>
     );
 }
